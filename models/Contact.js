57a5111a0d8467aefa253a9efd7636312e6fb064
Андрій <rukom@ukr.net>
2024-03-15 17:49:50 +0200

    Update Contact.js

--- models/Contact.js
+++ models/Contact.js
@@ -16,8 +16,7 @@ const contactSchema = new Schema({
       type: Boolean,
       default: false,
     },
-  }
-);
+  }, {versionKey: false, timestamps: true});
 
 contactSchema.post("save", handleSaveError);
 
