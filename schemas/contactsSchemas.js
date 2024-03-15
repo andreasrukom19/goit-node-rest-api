2d04bb2f854abac27f282e074c8684e19d9dac13
Андрій <rukom@ukr.net>
2024-03-15 18:00:24 +0200

    Update contactsSchemas.js

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -2,13 +2,12 @@ import Joi from "joi";
 
 export const createContactSchema = Joi.object({
   name: Joi.string().required(),
-  email: Joi.string(),
-  phone: Joi.string(),
-  favorite: Joi.boolean()
+  email: Joi.string().required(),
+  phone: Joi.string().required()
 })
 
 export const updateContactSchema = Joi.object({
-  name: Joi.string().required(),
+  name: Joi.string(),
   email: Joi.string(),
   phone: Joi.string(),
   favorite: Joi.boolean()
