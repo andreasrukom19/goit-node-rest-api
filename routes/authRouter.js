a8cddb85b007657744b864a2a0165f4bb0d5a428
Андрій <rukom@ukr.net>
2024-03-26 22:14:23 +0200

    Update authRouter.js

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -3,6 +3,7 @@ import authController from "../controllers/authController.js";
 import { validateBody } from "../helpers/validateBody.js";
 import { userSigninSchema, userSignupSchema } from "../schemas/usersSchemas.js";
 import authenticate from "../middlewares/authenticate.js";
+import upload from "../middlewares/upload.js";
 
 const authRouter = express.Router();
 
@@ -13,5 +14,7 @@ authRouter.post("/signin", validateBody(userSigninSchema), authController.signin
 authRouter.get("/current", authenticate, authController.getCurrent);
 
 authRouter.post("/signout", authenticate, authController.signout);
+
+authRouter.patch("/avatars", authenticate, upload.single("avatar"), authController.avatarUpdate);
 
 export default authRouter;
