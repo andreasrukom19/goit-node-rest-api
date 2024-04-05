2651700453ee8b47b1c8fb1ed6081e7f30433275
Андрій <rukom@ukr.net>
2024-04-05 16:45:59 +0300

    Update User.js

--- models/User.js
+++ models/User.js
@@ -29,6 +29,13 @@ const userSchema = new Schema({
   avatarURL: {
     type: String,
   },
+  verify: {
+    type: Boolean,
+    default: false,
+  },
+  verificationToken: {
+    type: String,
+  },
 }, { versionKey: false, timestamps: true });
 
 userSchema.post("save", handleSaveError);
